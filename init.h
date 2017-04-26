87306b0aa5c1bc36fb88c73fe39d6305cac5e0ae
dan947 <daniel.romero2096@gmail.com>
2017-04-26 18:59:25 -0500

    cosas

--- init.h
+++ init.h
@@ -5,5 +5,6 @@ physics_grid * create_physics_grid(void);
 U_grid * create_U_grid(void);
 F_grid * create_F_grid(void);
 void initMatrixP(physics_grid *P,int problem);
-void init_UandF(physics_grid *P,U_grid *U,F_grid *Fx,F_grid *Fy,F_grid *Fz,int problem);
+void init_Uc(physics_grid *P,U_grid *U)
+void init_Fc(physics_grid *P,F_grid *Fx,F_grid *Fy,F_grid *Fz,int problem);
 void init_problem(physics_grid *P,U_grid *U,F_grid *Fx,F_grid *Fy,F_grid *Fz,int problem);
