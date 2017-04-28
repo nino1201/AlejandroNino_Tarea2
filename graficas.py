8ec716e4e331e39ecb8f00c9f2018586acaa45ac
nino1201 <a.nino1201@gmail.com>
2017-04-28 11:52:21 -0500

    grafica

---
+++ graficas.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+datos=np.genfromtxt("euler.dat")
+
+r=np.array([10,60,120])
+plt.annotate("t= "+str(datos[0,1]),xy=(10,datos[0,0]),xytext=(5,datos[0,0]+1))
+plt.annotate("t= "+str(datos[1,1]),xy=(60,datos[1,0]),xytext=(55,datos[1,0]+1))
+plt.annotate("t= "+str(datos[2,1]),xy=(100,datos[2,0]),xytext=(115,datos[2,0]+1))
+
+
+plt.title("Rho vs r")
+plt.scatter(r,datos[:,0])
+plt.show()
